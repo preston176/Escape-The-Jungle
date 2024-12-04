34788eb9182df6468d609c89e46eae4e05a85424
Preston M <prestonnyamweya@gmail.com>
2024-12-04 16:46:23 +0300

    feat: implement infinite generation

--- Assets/Scripts/SegmentGenerator.cs
+++ Assets/Scripts/SegmentGenerator.cs
@@ -4,26 +4,29 @@ using UnityEngine;
 
 public class SegementGenerator : MonoBehaviour
 {
-    public GameObject segmentMap01;
-    public GameObject segmentMap02;
-    public GameObject segmentMap03;
-    public GameObject segmentMap04;
-    public GameObject segmentMap05;
-    public GameObject segmentMap06;
-    public GameObject segmentMap07;
+    public GameObject[] segment;
+    [SerializeField] int zPos = 50;
+    [SerializeField] bool creatingSegment = false;
+    [SerializeField] int segmentNum;
 
 
-    void Start()
+    void Update()
     {
-        StartCoroutine(SegmentGen());
+        if(creatingSegment == false)
+        {
+            creatingSegment = true;
+            StartCoroutine(SegmentGen());
+        }
+        
     }
 
     IEnumerator SegmentGen()
     {
-        yield return new WaitForSeconds(10);
-        segmentMap02.SetActive(true);
-        yield return new WaitForSeconds(10);
-        segmentMap03.SetActive(true);
+        segmentNum = Random.Range(0, 3);
+        Instantiate(segment[segmentNum], new Vector3(0, 0, zPos), Quaternion.identity);
+        zPos += 50;
+        yield return new WaitForSeconds(3);
+        creatingSegment = false;
     }
 
 }
